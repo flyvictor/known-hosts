f0181acc157d36f457b0a2fe1ec274d9d2ef4180
Craig Edmunds <craig@flyvictor,com>
2016-08-04 08:49:08 +0100

    Updated to support known hosts files in different locations

--- index.js
+++ index.js
@@ -2,31 +2,43 @@ var fs = require('fs');
 var path = require('path');
 var crypto = require('crypto');
 
-var HOME = process.env.HOME || process.env.USERPROFILE;
-var KNOWN_HOSTS = path.join(HOME, '.ssh', 'known_hosts');
 
-var md5 = function(host) {
-	return crypto.createHash('md5').update(host, 'base64').digest('hex');
-};
+function getKnownHosts(knownHostFile) {
 
-var data = fs.existsSync(KNOWN_HOSTS) ? fs.readFileSync(KNOWN_HOSTS, 'utf-8').trim().split('\n') : [];
-var entries = [];
+	if (!knownHostFile) {
+		var HOME = process.env.HOME || process.env.USERPROFILE;
+		knownHostFile = path.join(HOME, '.ssh', 'known_hosts');
+	}
 
-data.forEach(function(entry) {
-	var i = entry.indexOf(' ');
-	if (i === -1) return;
+	var md5 = function(host) {
+		return crypto.createHash('md5').update(host, 'base64').digest('hex');
+	};
 
-	var hosts = entry.slice(0, i).trim().split(',');
-	var key = entry.slice(i+1);
-	var fingerprint = md5(key.split(' ').pop());
+	var data = fs.existsSync(knownHostFile) ? fs.readFileSync(knownHostFile, 'utf-8').trim().split('\n') : [];
+	var entries = [];
 
-	hosts.forEach(function(host) {
-		entries.push({
-			host: host,
-			publicKey: key,
-			fingerprint: fingerprint
+	data.forEach(function(entry) {
+
+		var i = entry.indexOf(' ');
+		if (i === -1) return;
+
+		var hosts = entry.slice(0, i).trim().split(',');
+		var key = entry.slice(i+1);
+		var fingerprint = md5(key.split(' ').pop());
+		var type = key.split(' ')[0];
+
+		hosts.forEach(function(host) {
+
+			entries.push({
+				host: host,
+				publicKey: key,
+				fingerprint: fingerprint,
+				type: type
+			});
 		});
 	});
-});
 
-module.exports = entries;
+	return entries;
+
+}
+module.exports = { getKnownHosts : getKnownHosts };
